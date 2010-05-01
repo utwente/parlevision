9844df70b36a013047d76f9c259beae6e6ebf3f3
micheljansen <mail@micheljansen.org>
2010-05-01 18:21:26 +0200

    Added empty LibraryWidget and facilities to save and restore window state upon start/exit

---
+++ src/plvgui/LibraryWidget.cpp
@@ -0,0 +1,16 @@
+#include "include/plvgui/LibraryWidget.h"
+#include "ui_librarywidget.h"
+
+using namespace plvgui;
+
+LibraryWidget::LibraryWidget(QWidget *parent) :
+    QDockWidget(parent),
+    ui(new Ui::LibraryWidget)
+{
+    ui->setupUi(this);
+}
+
+LibraryWidget::~LibraryWidget()
+{
+    delete ui;
+}
