e0aedb42a0245a8f3509ae0c2335205025f445c1
micheljansen <mail@micheljansen.org>
2010-05-09 17:26:19 +0200

    initialisation of pipelinescene

--- src/plvgui/PipelineScene.cpp
+++ src/plvgui/PipelineScene.cpp
@@ -7,12 +7,23 @@
 using namespace plvgui;
 using namespace plv;
 
-PipelineScene::PipelineScene(plv::Pipeline *pipeline, QObject *parent) :
+PipelineScene::PipelineScene(plv::Pipeline* pipeline, QObject* parent) :
         QGraphicsScene(parent),
         m_pipeline(pipeline)
 {
+    std::list< RefPtr<PipelineElement> > elements = m_pipeline->getChildren();
+
+    // add all elements from the pipeline to this scene
+    for( std::list< RefPtr<PipelineElement> >::iterator itr = elements.begin()
+        ; itr != elements.end(); ++itr )
+    {
+        this->add(*itr);
+    }
+
+    // make sure future additions to pipeline get added as well
     connect(m_pipeline, SIGNAL(elementAdded(plv::RefPtr<plv::PipelineElement>)),
             this, SLOT(add(plv::RefPtr<plv::PipelineElement>)));
+
 }
 
 void PipelineScene::add(plv::PipelineElement *e)
