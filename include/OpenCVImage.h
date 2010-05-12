6adfef52b165700adca50517b7b0faabfe036d88
Richard Loos <r.a.loos@gmail.com>
2010-05-12 23:30:22 +0200

    Added inital version of image recycling by making an OpenCVImageFactory which uses an internal object pool

--- include/OpenCVImage.h
+++ include/OpenCVImage.h
@@ -1,37 +1,83 @@
 #ifndef OPENCVIMAGE_H
 #define OPENCVIMAGE_H
 
-#include <opencv/cv.h>
+#include <list>
+//#include <opencv/cv.h>
+
+typedef struct _IplImage IplImage;
 
 #include "Types.h"
 
-namespace plv {
-
-class OpenCVImage : public Data
+namespace plv
 {
-public:
-    int getId() const { return m_id; }
-    IplImage* getImage() const { return m_img; }
+    class OpenCVImage;
 
-    OpenCVImage( int id, IplImage* img );
-    OpenCVImage( const OpenCVImage& other );
+    class OpenCVImageFactory
+    {
+    public:
+        /** constructs an OpenCVImage from an existing buffer. Default operation
+          * is that the newly constructed OpenCVImage does not own the buffer,
+          * and that a copy is created of the contents of the buffer. When own
+          * is true, the newly constructed OpenCVImage becomes the owner of
+          * the buffer, meaning it can be deleted by the OpenCVImage. Deleting
+          * the buffer externally results in undefined behaviour.
+          */
+        OpenCVImage* getFromBuffer( IplImage* buffer, bool own = false );
 
-    OpenCVImage* deepcopy();
+        /** Will return an OpenCVImage with appropropriate format. Uses object
+          * pooling to recycle images which are no longer in use by the pipeline.
+          * @returns an OpenCVImage with appropriate format
+          */
+        OpenCVImage* get( int width, int height, int depth, int channels );
 
-    static const QString& getType();
+        /** Purges all objects which are not in used from the object pool */
+        void purge();
 
-    /** implementation of the isCompatible method of the Resource abstract
-      * base class
-      */
-    //virtual bool isCompatibleFormat( const ComplexData* other );
+        /** Purges all objects, even those in use (refcount > 1). Dangerous! */
+        void purgeAll();
 
-protected:
-    unsigned int m_id;
-    IplImage* m_img;
+        /** @returns the number of object in the object pool */
+        int numObjects();
 
-    virtual ~OpenCVImage();
-};
+        /** @returns the number of objects in the object pool which are in use */
+        int numObjectsInUse();
 
+        inline static OpenCVImageFactory* instance()
+        {
+            if( m_instance == 0 )
+                m_instance = new OpenCVImageFactory();
+            return m_instance;
+        }
+
+    private:
+        OpenCVImageFactory();
+        ~OpenCVImageFactory();
+
+    private:
+        static OpenCVImageFactory* m_instance;
+        std::list<OpenCVImage*> m_objectPool;
+    };
+
+    class OpenCVImage : public Data
+    {
+        friend class OpenCVImageFactory;
+
+    public:
+        int getId() const { return m_id; }
+        const IplImage* getImageReadOnly() const { return m_img; }
+        IplImage* getImage();
+        OpenCVImage* deepcopy();
+
+    protected:
+        OpenCVImage( IplImage* img );
+        OpenCVImage( const OpenCVImage& other );
+
+        virtual ~OpenCVImage();
+
+    protected:
+        unsigned int m_id;
+        IplImage* m_img;
+    };
 }
 
 #endif
