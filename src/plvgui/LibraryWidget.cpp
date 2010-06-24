25f743e1617a4c3f9a817911fdde7f3e022a789c
micheljansen <mail@micheljansen.org>
2010-06-24 22:37:06 +0200

    constrained list using a spacer

--- src/plvgui/LibraryWidget.cpp
+++ src/plvgui/LibraryWidget.cpp
@@ -11,14 +11,23 @@ LibraryWidget::LibraryWidget(QWidget *parent) :
     ui(new Ui::LibraryWidget)
 {
     ui->setupUi(this);
+
     std::list<QString> types = plv::PipelineElement::types();
 
     for(std::list<QString>::iterator iter = types.begin();
         iter != types.end(); iter++)
     {
+        for(int i = 0; i < 4; i++) {
         qDebug() << "LibraryWidget: Adding type " << *iter;
         QWidget* w = new QLabel(QString(*iter), this);
+        w->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        w->setStyleSheet("background: red;");
+        w->setMinimumSize(100,40);
+        w->setMaximumSize(100,40);
+//        w->adjustSize();
+        w->resize(100,40);
         ui->container->addWidget(w);
+        }
     }
 }
 
