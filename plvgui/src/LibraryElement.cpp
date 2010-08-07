e9f0f423caf76798ef29b955d76ed41ed12c7a4e
Richard Loos <r.a.loos@gmail.com>
2010-08-07 21:07:40 +0200

    Added copyright notice and LGPL license to all project files.Added GPL license file. Renamed LGPL license file.

--- plvgui/src/LibraryElement.cpp
+++ plvgui/src/LibraryElement.cpp
@@ -1,3 +1,24 @@
+/**
+  * Copyright (C)2010 by Michel Jansen and Richard Loos
+  * All rights reserved.
+  *
+  * This file is part of the plvgui module of ParleVision.
+  *
+  * ParleVision is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * ParleVision is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * A copy of the GNU General Public License can be found in the root
+  * of this software package directory in the file LICENSE.LGPL.
+  * If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #include "LibraryElement.h"
 
 #include <QtGui>
